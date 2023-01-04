288905d9cbc2da01d4e522747e15116c9cc9c2ba
Víctor Santocildes Prado <victorsantocildes@opendeusto.es>
2023-01-04 19:14:32 +0100

    Ultimo

--- programa_Hilos.py
+++ programa_Hilos.py
@@ -6,53 +6,65 @@ from mfrc522 import SimpleMFRC522
 import signal
 import MySQLdb
 
+#contador = 0
 
-db = MySQLdb.connect(host='localhost',user='pi',passwd='pi',db='DatosFabrica')
-cur = db.cursor()
 
 reader = SimpleMFRC522()
 GPIO.cleanup()
 
 
-contador = 0
+db = MySQLdb.connect(host='localhost',user='PYV',passwd='PYV',db='DatosFabrica')
+cur = db.cursor()
 
-def rfid():
+query = "TRUNCATE TABLE Contador"
+cur.execute(query)
+db.commit()
+
+def rfid():    
+    contador = 0
     #CODIGO de la tarjeta rfid
     while True:
         id , text = reader.read()
-        
         if id == 1002848969990:
-            contado += 1
-            print('Accediendo con la tarjeta') 
-            print('contador: ',contador) 
+            contador += 1
+            print('Accediendo con la tarjeta')
+            
+            print('contador: ',contador)
+            print('------------------')
             time.sleep(1)  
     
         if id == 649130914353:
             contador += 1
             print('Accediendo con el llavero')
+            #print('------------------')
             print('Contador: ',contador)
+            print('------------------')
             time.sleep(1)
 
         
         
-    cur.execute('''INSERT INTO Contador(contador) VALUES(%s,%s);''',(contador))
-    db.commit()
+        query = "INSERT INTO Contador (contador) VALUES (%s)"
+        cur.execute(query, (contador,))
+        db.commit()
     
     
 def temp_hum():
     #CODIGO de la temperatura y la huedad
     while True:
+        print('------------------')
         print('Leyedo temperatura y humedad')
         humidity, temperature = Adafruit_DHT.read_retry(11,18)
         
         print('Temp = {0:0.1f}* Humidity = {1:0.1f}%'.format(temperature, humidity))
-        time.sleep(1)
+        print('------------------')
+        time.sleep(5)
         
         
-    cur.execute('''INSERT INTO Temp_Hum(humidity, temperature) VALUES(%s,%s);''',(humidity,temperature))
-    db.commit()
+        cur.execute('''INSERT INTO Temp_Hum(humidity, temperature) VALUES(%s,%s);''',(humidity,temperature))
+        db.commit()
     
 if __name__ == '__main__':
+    
     #Creamos los hilos
     t1 = threading.Thread(target=rfid)
     t2 = threading.Thread(target=temp_hum)
